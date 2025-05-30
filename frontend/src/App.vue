93f57a72ce457529b28afe90b8287763f2c89b8c
Karinigam <karinigam03@gmail.com>
2025-05-30 17:34:57 +0530

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <Navbar />
+    <!-- Show Navbar only if not on login page -->
+    <Navbar v-if="!isLoginPage" />
     <router-view />
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
+import Navbar from './components/Navbar.vue';
+import { useRoute } from 'vue-router';
 
 export default {
   name: 'App',
   components: {
     Navbar
+  },
+  setup() {
+    const route = useRoute();
+
+    const isLoginPage = computed(() => route.path === '/login');
+
+    return {
+      isLoginPage
+    };
   }
-}
+};
 </script>
 
 <style>
